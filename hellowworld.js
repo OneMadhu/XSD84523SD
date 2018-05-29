c6b8bdf92926bb3cbda00149d3eace3fedefa2ee
Madhu B <24282454+OneMadhu@users.noreply.github.com>
2018-05-29 19:15:40 +0530

    UI Features
    added tabline

--- hellowworld.js
+++ hellowworld.js
@@ -1,57 +1,48 @@
-/*console.logCopy = console.log.bind(console);
-console.log = function(data)
-{
-       if (arguments.length)
+function StartAction()
     {
         var currentTime = new Date();
         var currentOffset = currentTime.getTimezoneOffset();
-        var currentDate = '[' + new Date(currentTime.getTime() + (330 + currentOffset)*60000) + '] ';
-        this.logCopy(currentDate, data);
-    }
-};*/
-function StartAction()
-    {
-        
-		console.log("%c data: 1. StartAction Called","background: #222; color: #bada55");
-		console.log($('.notif_window')[0]);
+        var currentDate = '' + new Date(currentTime.getTime() + (330 + currentOffset)*60000) + '';
+		console.log("%cdata: 1. StartAction Called on " + currentDate,"background: #222; color: #bada55");
+		//console.log($('.notif_window')[0]);
         //if($('.notif_window')[0].is(':visible')) {    
-            console.log("data: 2. notification details are visible");
-            var obj =$("#buy");
-            console.log($("#buy"));
+            console.log("\tdata: 2. notification details are visible");
+            var objBUY =$("#buy");
+            console.log("\tdata: 3. Buy:" +  $("#buy"));
             //Buy
-            console.log("data: 3. Buy button length" + obj.length);
-            if(obj.length >0)
+            console.log("\t\tdata: 3.1. Buy button length" + objBUY.length);
+            if(objBUY.length >0)
             {
 				
-				obj = obj[0];
-				console.log("data: 4. Found Buy button," + obj);                
+				objBUY = objBUY[0];
+				console.log("\t\tdata: 3.2. Found Buy button," + objBUY);                
                 var e = document.createEvent('MouseEvents');
                 e.initEvent('click', true, true );                
-				obj.click(e);
-				console.log("%c data: 4. Buy button clicked!","background: #2AA206; color: #FFFFFF");
+				objBUY.click(e);
+				console.log("\t\t%cdata: 3.3. Buy Button clicked!","background: #2AA206; color: #FFFFFF");
             }
             else
             {
-                console.log("data: 4. Buy button not found");
+                console.log("\t\tdata: 3.2. Buy button not found");
 			}
 			//Sell
 			var objSELL =$("#sell");
-			console.log($("#sell"));
+			console.log("\tdata: 4. Sell:" +  $("#sell"));
             //alert(obj);
-            console.log("data: 3. SELL button length" + objSELL.length);
+            console.log("\t\tdata: 4.1 SELL button length" + objSELL.length);
             if(objSELL.length >0)
             {
 				
 				objSELL = objSELL[0];
-				console.log("data: 4. Found SELL button," + objSELL);                
+				console.log("\t\tdata: 4.2 Found SELL button," + objSELL);                
                 var eSELL = document.createEvent('MouseEvents');
                 eSELL.initEvent('click', true, true );                
 				objSELL.click(eSELL);
-				console.log("%c data: 4. SELL button clicked!","background: #D51206; color: #FFFFFF");
+				console.log("\t\t%cdata: 4.3 SELL button clicked!","background: #D51206; color: #FFFFFF");
             }
             else
             {
-                console.log("data: 4. SELL button not found");
+                console.log("\t\tdata: 4.2 SELL button not found");
             }
         //}
         //else
@@ -59,7 +50,7 @@ function StartAction()
         //    console.log("data: 2. notifications are not visible");
 
         //}
-    }
+}
 $("body").on('DOMSubtreeModified', "#notifications", function() {
 	console.log("data: There is change in Alert notifications");
 	StartAction();
